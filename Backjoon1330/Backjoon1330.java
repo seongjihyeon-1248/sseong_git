ae9a7191ec06590d99e8fde3809e942038e83e0a
seongjihyeon <sseongl6w6l@naver.com>
2022-01-26 22:52:57 +0900

    20220126

--- Backjoon1330/Backjoon1330.java
+++ Backjoon1330/Backjoon1330.java
@@ -4,8 +4,8 @@ public class Backjoon1330{
         int A, B;
         A = 1;
         B = 2;
-        if(A < B)           System.out.println("<");
-        else if (A > B)     System.out.println(">");
-        else                System.out.println("==");
+        if(A < B)           System.out.print("<");
+        else if (A > B)     System.out.print(">");
+        else                System.out.print("==");
     }                                   
 }
--- Backjoon1330/Backjoon1330.py
+++ Backjoon1330/Backjoon1330.py
@@ -1,5 +1,5 @@
-A = 1;
-B = 2;
-if A < B:       print("<");
-elif A > B:     print(">");
-else:           print("==");
+A = 1
+B = 2
+if A < B:       print("<")
+elif A > B:     print(">")
+else:           print("==")
