02bc32cd579ffe8e0bd00da485dceb206bcd9f05
seongjihyeon <sseongl6w6l@naver.com>
2022-02-16 15:56:04 +0900

    20220216

---
+++ Backjoon2438/Backjoon2438.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int N;
+    N = 5;
+
+    for(int i = 1; i < N + 1; i++){
+        for(int ii = 0; ii < i; ii++)
+            printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
--- Backjoon2741/Backjoon2741.c
+++ Backjoon2741/Backjoon2741.c
@@ -3,6 +3,9 @@
 int main(){
     int N;
     N = 5;
+    
     for(int i = 1; i <= 5; i++)
         printf(i);
+
+    return 0;
 }
