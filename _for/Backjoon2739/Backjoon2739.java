faa1b84a2fba6d54f35108dbb1882713d3a7b514
seongjihyeon <sseongl6w6l@naver.com>
2022-06-29 15:23:14 +0900

    20220629

---
+++ _for/Backjoon2739/Backjoon2739.java
@@ -0,0 +1,11 @@
+package _for.Backjoon2739;
+
+public class Backjoon2739 {
+    public static void main(String args[]){
+        int N;
+        N = 2;
+        for(int i = 1; i < 10; i++){
+            System.out.println(N +" * "+ i + " = " + N * i);
+        }
+    }
+}
