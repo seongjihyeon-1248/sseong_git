51cf48d1733dbffc61e6889faf4b42fda3c93d6a
seongjihyeon <sseongl6w6l@naver.com>
2022-08-11 11:00:47 +0900

    20220811

---
+++ Backjoon2231/Backjoon2231.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int N = 216;
+    for(int i = 1; i < N; i++){
+        int temp = i;
+        int num = i;
+        while(temp > 0){
+            num += temp % 10;
+            temp /= 10;
+        }
+        if(num == N){
+            printf("%d", i);
+            N = 0;
+            break;
+        }
+    }
+    if(N != 0)
+        printf("0");
+}
