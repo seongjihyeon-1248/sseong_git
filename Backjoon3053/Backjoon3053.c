332cb26e3684da37bbc703ba893904a67d1e4ab1
seongjihyeon <sseongl6w6l@naver.com>
2022-09-12 22:19:36 +0900

    20220912

---
+++ Backjoon3053/Backjoon3053.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+int main(){
+    float R = 21;
+    printf("%0.6f\n", R * R * M_PI);
+    printf("%0.6f", R * R * 2);
+}
---
+++ Backjoon3053/Backjoon3053.py
@@ -0,0 +1,4 @@
+import math
+R = 1
+print(format(((R ** 2) * math.pi), ".6f"))
+print(format(((R ** 2) * 2), ".6f"))
