c533404d0f022c0556993143f484621c4ab03c54
seongjihyeon <sseongl6w6l@naver.com>
2022-12-28 17:28:58 +0900

    20221228

--- 20221227/20221227.py
+++ 20221227/20221227.py
@@ -4,9 +4,12 @@ from collections import Counter
 
 def solution(k, tangerine):
     answer = 0
-    count = sorted(Counter(tangerine).items(),reverse = True, key = lambda x : x[1])
-    for key, value in count:
-        if k <= 0: break
-        k -= value
+    count = sorted(Counter(tangerine).values(), reverse = True)
+    for i in count:
+        k -= i
         answer += 1
+        if k <= 0:
+            break
     return answer
+
+print(solution(6, [1, 3, 2, 5, 4, 5, 2, 3]))
---
+++ 20221228/20221228.py
@@ -0,0 +1,21 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/138475?language=python3
+
+import math
+
+def solution(e, starts):
+    answer = []
+    count_list = []
+    count_list.append(1)
+    for count in range(2, e + 1):
+        cnt = 0
+        for c in range(1, math.floor(count**(1/2)) + 1):
+            if(count % c  == 0):
+                  cnt += 2
+        if(math.floor(count**(1/2))**2 == count): 
+            cnt -= 1
+        count_list.append(cnt)
+    for s in starts:
+        answer.append(count_list[s - 1:e].index(max(count_list[s - 1:e])) + s)
+    return answer
+    
+print(solution(8, [1, 3, 7]))
