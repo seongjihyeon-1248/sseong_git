992a78552192f1ef9a525bfb9c7b7d3cbe1b533c
seongjihyeon <sseongl6w6l@naver.com>
2023-01-04 13:59:29 +0900

    20230104

--- 20230103/20230103.py
+++ 20230103/20230103.py
@@ -1,23 +1,19 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/132265
-# 시간 초과 해결 못함...
 
 from collections import Counter
 
 def solution(topping):
-    return ans(topping, 0, len(topping) - 1)
-
-def ans(topping, min, max):
-    if(max < min): 
-        return 0
-    mid = (min + max) // 2
-    len_min = len(Counter(topping[:mid]))
-    len_max = len(Counter(topping[mid:]))
-    if(len_min > len_max):
-        return ans(topping, min, mid - 1)
-    elif(len_min < len_max):
-        return ans(topping, mid + 1, max)
-    else:
-        return 1 + ans(topping, min, mid - 1) + ans(topping, mid + 1, max)
-    
-
+    answer = 0
+    dic = Counter(topping)
+    set_dic = set()
+    for i in topping:
+        if dic[i] == 1:
+            dic.pop(i)
+        else:
+            dic[i] -= 1
+        set_dic.add(i)
+        if len(dic) == len(set_dic):
+            answer += 1
+    return answer
+ 
 print(solution([1, 2, 1, 3, 1, 4, 1, 2]))
