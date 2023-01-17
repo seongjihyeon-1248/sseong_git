3265daf59f79e114792cbae38f95e6d947e43b7c
seongjihyeon <sseongl6w6l@naver.com>
2023-01-17 17:49:54 +0900

    20230117

--- _20230110/_20230110.java
+++ _20230110/_20230110.java
@@ -11,8 +11,10 @@ public class _20230110{
 class Solution {
     public int solution(String t, String p) {
         int answer = 0;
-        int len_p = p.length();
-        for (int i = 0; i < t.length() - len_p + 1; i++) {
+        //p문자열의 길이를 받음
+        int len_p = p.length();  
+        //t문자열을 p길이 만큼 앞부터 잘라서 p문자열과 크기 비교하여 작거나 같으면 카운트 증가
+        for (int i = 0; i < t.length() - len_p + 1; i++) { 
             if (t.substring(i, i + len_p).compareTo(p) <= 0) 
                 answer++;
         }
