e146273b2edbe4a7b5292e8435d8987721e85f57
seongjihyeon <sseongl6w6l@naver.com>
2023-01-24 21:11:56 +0900

    20230124

---
+++ _20230124/_20230124.java
@@ -0,0 +1,17 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/17682
+
+package _20230124;
+
+import java.util.*;
+
+public class _20230117{
+    public static void main(String args[]){
+        System.out.println(new Solution().solution("1S2D*3T"));
+    }
+}
+class Solution {
+    public int solution(String dartResult) {
+        int answer = 0;
+        return answer;
+    }
+}
