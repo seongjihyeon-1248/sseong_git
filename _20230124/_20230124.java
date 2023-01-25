fef65907f5e45c23646e7028ce2682e4f8bd2556
seongjihyeon <sseongl6w6l@naver.com>
2023-01-26 01:17:00 +0900

    20230126

--- _20230124/_20230124.java
+++ _20230124/_20230124.java
@@ -1,18 +1,55 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/17682
 
 package _20230124;
-
+import java.util.*;
 public class _20230124{
     public static void main(String args[]){
-        System.out.println(new Solution().solution("1S2D*3T"));
+        System.out.println(new Solution().solution("1D2S0T"));
     }
 }
 class Solution {
     public int solution(String dartResult) {
         int answer = 0;
+        ArrayList<Integer> A = new ArrayList<Integer>();
+        int a = -1;
+        int A_get = 0;
         for(int i = 0; i < dartResult.length(); i++){
-            System.out.println(dartResult.substring(i, i + 1));
+            String sub = dartResult.substring(i, i + 1);
+            switch(sub){
+                case "S":
+                    break;
+                case "D": 
+                    A_get = A.get(a);
+                    A.set(a, A_get * A_get);
+                    break;
+                case "T":
+                    A_get = A.get(a);
+                    A.set(a, A_get * A_get * A_get);
+                    break;
+                case "*":
+                    A.set(a, A.get(a) * 2);
+                    if(a != 0)
+                        A.set(a - 1, A.get(a - 1) * 2);
+                    break;
+                case "#":
+                    A.set(a, A.get(a) * -1);
+                    break;
+                case "0":
+                    if(i != 0 && (dartResult.substring(i - 1, i)).compareTo("1") == 0)
+                        A.set(a, 10);
+                    else{
+                        A.add(0);
+                        a++;
+                    }
+                    break;
+                default:
+                    A.add(Integer.parseInt(sub));
+                    a++;
+                    break;
+            }
         }
+        for(int i : A)
+            answer += i;
         return answer;
     }
 }
