3e3c8ced654953bc97aca4eee092508cac7949ae
seongjihyeon <sseongl6w6l@naver.com>
2023-02-03 20:23:26 +0900

    202302023

--- test/test.java
+++ test/test.java
@@ -1,6 +1,12 @@
 package test;
 public class test{
     public static void main(String args[]){
-        System.out.println("test");
+        System.out.println(new Solution().solution());
     }                                   
+}
+
+class Solution {
+    public int solution() {
+        return 0;
+    }
 }
