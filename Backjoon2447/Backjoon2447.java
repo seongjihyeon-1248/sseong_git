81e35e83bdfd3e6971f0d9b1d1d67f369cadcabe
seongjihyeon <sseongl6w6l@naver.com>
2023-02-09 01:37:44 +0900

    20230209

--- Backjoon2447/Backjoon2447.java
+++ Backjoon2447/Backjoon2447.java
@@ -1,14 +1,19 @@
 package Backjoon2447;
 
+// 사실 제대로 이해하지 못해서 그냥 외움
+
 class Backjoon2447{
     public static void starboxMaker(int row, int col, int N){
+        //row를 N으로 나눴을 때의 나머지가 1이면 빈 공간임
         if ((row / N) % 3 == 1 && (col / N) % 3 == 1) {
             System.out.print(" ");
         }
         else {
-            if (N / 3 == 0) {
+            //N이 3보다 작아지면 이제 row / N의 나머지가 1이 될 가능 성이 없으므로 *출력
+            if (N < 3) {
                 System.out.print("*");
             }
+            //N이 3보다 크면 N을 3으로 나누고 재귀문 반복
             else {
                 starboxMaker(row, col, N / 3);
             }
