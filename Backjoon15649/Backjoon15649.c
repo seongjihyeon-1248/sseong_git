c7cf55238336bcf93479ff8d12e049d7367f8084
seongjihyeon <sseongl6w6l@naver.com>
2023-02-14 17:49:37 +0900

    20230214

---
+++ Backjoon15649/Backjoon15649.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void recursion(int idx, int N, int M, int* arr, int* isUsed) {
+        if (idx == M) {
+            for (int i = 0; i < M; i++) {
+                printf("%d ", arr[i]);
+            }
+            printf("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (isUsed[i] == 0) {
+                isUsed[i] = 1;
+                arr[idx] = i;
+                recursion(idx + 1, N, M, arr, isUsed); 
+                isUsed[i] = 0;
+            }
+        }
+    }
+
+int main(){
+    int N = 4;
+    int M = 2;
+    int*isUsed = (int*)malloc(sizeof(int) * (N + 1));
+    int*arr = (int*)malloc(sizeof(int) * M);
+    for (int i = 0; i <= N; i++){
+         isUsed[i] = 0;
+    }
+    recursion(0, N, M, arr, isUsed);
+}
---
+++ Backjoon15649/Backjoon15649.py
@@ -0,0 +1,22 @@
+def recursion(idx, N, M, arr, isUsed):
+    if (idx == M):
+        for i in arr:
+            print(i, end=" ")
+        print()
+        return
+
+    for i in range(1, N + 1):
+        if (isUsed[i] == 0):
+            isUsed[i] = 1
+            arr.append(i)
+            recursion(idx + 1, N, M, arr, isUsed); 
+            isUsed[i] = 0
+            arr.pop()
+
+N = 4
+M = 2
+isUsed = list()
+arr = list()
+for i in range(0, N + 1):
+    isUsed.append(0)
+recursion(0, N, M, arr, isUsed)
