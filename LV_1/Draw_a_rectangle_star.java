ab81dbfcf0c6d185da99943aa3f8c8df5f2c7aed
seongjihyeon <sseongl6w6l@naver.com>
2023-02-23 12:43:08 +0900

    20230223

---
+++ LV_1/Draw_a_rectangle_star.java
@@ -0,0 +1,17 @@
+package LV_1;
+
+import java.util.Scanner;
+
+public class Draw_a_rectangle_star{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for(int i = 0; i < b; i++){
+            for(int ii = 1; ii < a; ii++){
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+    }
+}
