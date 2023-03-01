710f56e89d07e7c7922881af77ba6be9f982d97c
seongjihyeon <sseongl6w6l@naver.com>
2023-03-01 20:59:52 +0900

    20230301

--- Backjoon10872/Backjoon10872.java
+++ Backjoon10872/Backjoon10872.java
@@ -1,13 +1,14 @@
 package Backjoon10872;
-
+import java.util.Scanner;
 public class Backjoon10872 {
     public static int fac(int n){
-        if(n == 1)
-            return 1;
+        if(n <= 1) return 1;
         return n * fac(n - 1);
     }
     public static void main(String args[]){
-        int N = 10;
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        in.close();
         System.out.print(fac(N));
     }
 }
