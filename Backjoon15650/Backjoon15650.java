53f4a27cbe5d904012075a1f532a0c0e7c0f7937
seongjihyeon <sseongl6w6l@naver.com>
2023-03-09 15:15:44 +0900

    20230309

--- Backjoon15650/Backjoon15650.java
+++ Backjoon15650/Backjoon15650.java
@@ -1,10 +1,17 @@
 package Backjoon15650;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Backjoon15650 {
 
-    public static void main(String[] args) {
-        int N = 3;
-        int M = 1;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         boolean[] isUsed = new boolean[N + 1];
         int[] arr = new int[M];  
 
