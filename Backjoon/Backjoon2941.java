7cb351ea61870909da1d80b6062be546f225a935
seongjihyeon <sseongl6w6l@naver.com>
2023-03-22 13:29:08 +0900

    20230322

---
+++ Backjoon/Backjoon2941.java
@@ -0,0 +1,18 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Backjoon2941 {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        int count = 0;
+        String[] cro = new String[]{"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        for(String i : cro) 
+            a = a.replaceAll(i," ");
+        count += a.length();
+        System.out.print(count);
+    }
+}
