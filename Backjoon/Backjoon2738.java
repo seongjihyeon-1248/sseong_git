08b31f93fc9c1f2b97693b6a1856ee337b11a1bb
seongjihyeon <sseongl6w6l@naver.com>
2023-03-24 00:15:45 +0900

    20230324

---
+++ Backjoon/Backjoon2738.java
@@ -0,0 +1,36 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Backjoon2738{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); 
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] A = new int[N][M];
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int ii = 0; ii < M; ii++){
+                A[i][ii] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int ii = 0; ii < M; ii++){
+                A[i][ii] += Integer.parseInt(st.nextToken());
+            }
+        }
+        for(int i = 0; i < N; i++){
+            for(int ii = 0; ii < M; ii++){
+                System.out.print(A[i][ii] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
