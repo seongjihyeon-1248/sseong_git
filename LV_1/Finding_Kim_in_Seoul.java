7b8fe2aba9402139766332b78aa9b74251d23e09
seongjihyeon <sseongl6w6l@naver.com>
2023-04-04 17:17:31 +0900

    20230404

---
+++ LV_1/Finding_Kim_in_Seoul.java
@@ -0,0 +1,18 @@
+package LV_1;
+
+class Finding_Kim_in_Seoul{
+    public static void main(String args[]){
+        System.out.println(new Solution_Finding_Kim_in_Seoul().solution(new String[]{"Jane", "Kim"}));
+    }
+}
+
+class Solution_Finding_Kim_in_Seoul {
+    public String solution(String[] seoul) {
+        int i;
+        for(i = 0; i < seoul.length; i++)
+            if(seoul[i].equals("Kim"))
+                break;
+
+        return "김서방은 " + i + "에 있다";
+    }
+}
