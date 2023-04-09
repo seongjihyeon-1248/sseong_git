3823a8bad008f9d2bc2220df9f6ad8d5eecd683e
seongjihyeon <sseongl6w6l@naver.com>
2023-04-09 19:14:03 +0900

    20230409

---
+++ LV_0/The_product_of_two_numbers.java
@@ -0,0 +1,13 @@
+package LV_0;
+
+public class The_product_of_two_numbers {
+    public static void main(String args[]){
+        System.out.println(new Solution_The_product_of_two_numbers().solution(3,4));
+    }
+}
+
+class Solution_The_product_of_two_numbers  {
+    public int solution(int num1, int num2) {
+        return num1 * num2;
+    }
+}
