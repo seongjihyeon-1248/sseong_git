a00dccef8ec4f4a5189ae572eb9440d6788a8ca5
seongjihyeon <sseongl6w6l@naver.com>
2023-07-01 23:25:20 +0900

    20230701

---
+++ LV_2/Change_number.java
@@ -0,0 +1,14 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/154538
+package LV_2;
+
+public class Change_number {
+    public static void main(String args){
+    }
+}
+
+class Solution_Change_number {
+    public int solution(int x, int y, int n) {
+        int answer = 0;
+        return answer;
+    }
+}
