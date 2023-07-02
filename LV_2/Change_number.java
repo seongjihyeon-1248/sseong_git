115dab0d89dc6f40752265c1280d12f164d54511
seongjihyeon <sseongl6w6l@naver.com>
2023-07-03 01:23:35 +0900

    20230703

--- LV_2/Change_number.java
+++ LV_2/Change_number.java
@@ -1,46 +1,30 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/154538
 package LV_2;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class Change_number {
     public static void main(String args[]){
-        System.out.println(new Solution_Change_number().solution(10, 40, 5));
+        System.out.println(new Solution_Change_number().solution(10, 1000000, 5));
     }
 }
 
 class Solution_Change_number {
-    int n;
-    int y;
-    ArrayList<Integer> list = new ArrayList<Integer>();
     public int solution(int x, int y, int n) {
-        this.n = n;
-        this.y = y;
-        cal(x, 0);
-        Collections.sort(list);
-        if(list.size() > 0) return list.get(0);
-        return -1;
-    }
-    void cal(int a, int count){
-        count++;
-        add(a, count);
-        two(a, count);
-        three(a, count);
-    }
-    void add(int a, int count){
-        a += n;
-        if(a < y)       cal(a, count);
-        else if(a == y) list.add(count);
-    }
-    void two(int a, int count){
-        a *= 2;
-        if(a < y)       cal(a, count);
-        else if(a == y) list.add(count);
-    }
-    void three(int a, int count){
-        a *= 3;
-        if(a < y)       cal(a, count);
-        else if(a == y) list.add(count);
+        int[] dp = new int[y + 1];
+        for (int i = x; i < y + 1; i++) {
+            if (i != x && dp[i] == 0) {
+                dp[i] = -1;
+                continue;
+            }
+            if (i * 2 <= y) {
+                dp[i * 2] = (dp[i * 2] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i * 2]);
+            }
+            if (i * 3 <= y) {
+                dp[i * 3] = (dp[i * 3] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i * 3]);
+            }
+            if (i + n <= y) {
+                dp[i + n] = (dp[i + n] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i + n]);
+            }
+        }
+        return dp[y];
     }
 }
