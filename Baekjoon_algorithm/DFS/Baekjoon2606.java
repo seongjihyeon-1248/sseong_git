b49e1118331e0588f5a6c0d22f7de495bb589abc
seongjihyeon <sseongl6w6l@naver.com>
2023-07-05 15:41:31 +0900

    20230705

--- Baekjoon_algorithm/DFS/Baekjoon2606.java
+++ Baekjoon_algorithm/DFS/Baekjoon2606.java
@@ -1,17 +1,15 @@
-package BFS;
+package DFS;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Baekjoon2606 {
     static Boolean[][] list;
     static HashSet<Integer> route;
-    static HashSet<Integer> visited;
+    static HashSet<Integer> notyet;
     static int num;
 
     public static void main(String args[]) throws IOException {
@@ -20,9 +18,9 @@ public class Baekjoon2606 {
         list = new Boolean[++num][num];
         int line = Integer.parseInt(br.readLine());
         route = new HashSet<>();
-        visited = new HashSet<>();
+        notyet = new HashSet<>();
         for (int i = 1; i < num; i++)
-            visited.add(i);
+            notyet.add(i);
 
         // 간선 배열 초기화
         for (int i = 0; i < line; i++) {
@@ -32,29 +30,19 @@ public class Baekjoon2606 {
             list[a][b] = true;
             list[b][a] = true;
         }
-
-        bfs(1);
+        dfs(1);
         System.out.print(route.size() - 1);
     }
-
-    static void bfs(int start) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(start);
-        visited.remove(start);
-
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-            route.add(current);
-
-            HashSet<Integer> toRemove = new HashSet<>();
-            for (int neighbor : visited) {
-                if (list[current][neighbor] != null) {
-                    queue.offer(neighbor);
-                    toRemove.add(neighbor);
-                }
+    
+    static void dfs(int i) {
+        route.add(i);
+        notyet.remove(i);
+        HashSet<Integer> toRemove = new HashSet<>();
+        for (int ch : new HashSet<>(notyet)) 
+            if (list[i][ch] != null) {
+                toRemove.add(ch);
+                dfs(ch);
             }
-
-            visited.removeAll(toRemove);
-        }
+        notyet.removeAll(toRemove);
     }
 }
