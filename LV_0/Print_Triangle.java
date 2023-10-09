415671eb566ae10af2b15237e12e510811992247
seongjihyeon <sseongl6w6l@naver.com>
2023-10-09 22:20:21 +0900

    Update LV_0

--- LV_0/Print_Triangle.java
+++ LV_0/Print_Triangle.java
@@ -7,6 +7,7 @@ public class Print_Triangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        sc.close();
         for(int i = 0; i < n; i++){
             for(int ii = 0; ii <= i; ii++){
                 System.out.print("*");
