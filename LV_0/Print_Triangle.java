577ea5b40394535803156e275c3e3ef831996352
seongjihyeon <sseongl6w6l@naver.com>
2023-10-09 22:19:21 +0900

    Add LV_0

---
+++ LV_0/Print_Triangle.java
@@ -0,0 +1,17 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/120823
+package LV_0;
+
+import java.util.Scanner;
+
+public class Print_Triangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            for(int ii = 0; ii <= i; ii++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
