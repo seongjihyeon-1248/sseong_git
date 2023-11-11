9b4e4a908dd1c41b8f4f4b8b75565408512354e0
seongjihyeon <sseongl6w6l@naver.com>
2023-11-12 00:24:39 +0900

    20231112

--- LV_3/Return_place.java
+++ LV_3/Return_place.java
@@ -1,50 +1,76 @@
 package LV_3;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Return_place {
     public static void main(String args[]){
-        System.out.println(new Solution_Return_place().solution(3, new int[][]{{1,2},{2,3}}, new int[]{2,3}, 1));
+        System.out.println(new Solution_Return_place().solution(5, new int[][]{{1,2},{1,4},{2,4},{2,5},{4,5}}, new int[]{1,3,5}, 5));
     }
 }
 
 class Solution_Return_place {
-    int[][] distance;
+    int totalNode;
+    int gDestination;
+    Map<Integer, Integer> saveMap;
+    int[] distances;
+
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
-        ArrayList<Integer> d = new ArrayList<>();
-        int[] answer = new int[sources.length];
-        distance = new int[n+1][n+1];
-        for(int i = 1; i < n + 1; i++){ 
-            for(int ii = 1; ii < n + 1; ii++){  
-                distance[i][ii] = -1;
+        totalNode = n;
+        gDestination = destination;
+        saveMap = new HashMap<>();
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] road : roads) {
+            graph.get(road[0]).add(road[1]);
+            graph.get(road[1]).add(road[0]);
+        }
+        distances = new int[n + 1];
+        Arrays.fill(distances, Integer.MAX_VALUE);
+
+        dijkstra(sources, graph);
+
+        List<Integer> answer = new ArrayList<>();
+        for (int source : sources) {
+            answer.add(distances[source] == Integer.MAX_VALUE ? -1 : distances[source]);
+        }
+
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+
+    private void dijkstra(final int[] sources, final List<List<Integer>> graph) {
+        Deque<Info> deque = new ArrayDeque<>();
+        deque.addLast(new Info(gDestination, 0));
+        distances[gDestination] = 0;
+
+        while (!deque.isEmpty()) {
+            final Info info = deque.pollFirst();
+            int curValue = info.value;
+            int curDistance = info.distance;
+
+            if (distances[curValue] != curDistance) {
+                continue;
+            }
+
+            for (int next : graph.get(curValue)) {
+                if (distances[next] > curDistance + 1) {
+                    distances[next] = curDistance + 1;
+                    deque.addLast(new Info(next, curDistance + 1));
+                }
             }
         }
-        for(int i = 1; i < n + 1; i++){  
-            distance[i][i] = 0;
-            d.add(i);
+    }
+
+    class Info {
+        int value;
+        int distance;
+
+        Info(int value, int distance) {
+            this.value = value;
+            this.distance = distance;
         }
-        for(int[] r: roads){
-            distance[r[0]][r[1]] = 1;
-            distance[r[1]][r[0]] = 1;
-        }
-        d.remove(destination);
-        for(int i = 0; i < n - 1; i++){
-            int min = d.get(0);
-            for(int D: d){
-                if(distance[destination][D] != -1 && distance[destination][D] < distance[destination][min])
-                    min = D;
-            }
-            System.out.println(min);
-            d.remove(min);
-            for(int D: d){
-                if(distance[min][D] != -1 && distance[destination][D] > distance[destination][min] + distance[min][D])
-                    distance[destination][D] = distance[destination][min] + distance[min][D];
-            }
-        }
-        for(int i = 0; i < sources.length; i++){
-            answer[i] =  distance[destination][sources[i]];
-            System.out.println(answer[i]);
-        }
-        return answer; 
     }
 }
