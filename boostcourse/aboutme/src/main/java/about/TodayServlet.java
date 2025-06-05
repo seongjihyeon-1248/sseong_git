beca05a848faaa9a618042dcc49a987cbd799a58
seongjihyeon <sseongl6w6l@naver.com>
2025-06-05 23:04:28 +0900

    20250625

--- boostcourse/aboutme/src/main/java/about/TodayServlet.java
+++ boostcourse/aboutme/src/main/java/about/TodayServlet.java
@@ -7,6 +7,9 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Servlet implementation class TodayServlet
@@ -30,9 +33,32 @@ public class TodayServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//TODO Auto-generated method stub
 		//먼저 aboutme.html로 열리게 함
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/aboutme.html"); 
+		/*
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/index.html"); 
 		System.out.println("restart");
 		dispatcher.forward(request, response); 
+		*/
+		response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        // 현재 시각 가져오기
+        LocalDateTime now = LocalDateTime.now();
+        // 원하는 형식으로 포맷
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+        String formattedNow = now.format(formatter);
+        
+        // HTML 출력
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head><title>현재 시각</title></head>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<a href='index.html'> 메인화면</a>");
+        out.println("<div>");
+        out.println("<h1>현재 시간: " + formattedNow + "</h1>");
+        out.println("</div>");
+        out.println("</body>");
+        out.println("</html>");
 	}
 
 }
