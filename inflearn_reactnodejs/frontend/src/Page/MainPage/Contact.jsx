43498727412a0f8be184541fb997ec20743c0f45
seongjihyeon <sseongl6w6l@naver.com>
2025-11-27 00:17:10 +0900

    update mainpage

--- inflearn_reactnodejs/frontend/src/Page/MainPage/Contact.jsx
+++ inflearn_reactnodejs/frontend/src/Page/MainPage/Contact.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="bg-white py-20 lg:py-40">
+      <div className="container mx-auto px-4 max-w-6xl">
+        <div className="text-center mb-12">
+          <h2 className="text-4xl lg:text-5xl font-bold text-gray-800 mb-4">
+            문의하기
+          </h2>
+          <p className="text-gray-600 text-lg">
+            궁금하신 점이 있으신가요? 언제든 문의해주세용가리~
+          </p>
+        </div>
 
-export default Contact
+        <div className="grid md:grid-cols-3 gap-6 mb-20">
+          {[
+            {
+              title: "전화 문의",
+              info: "010-2002-0106",
+              subInfo: "평일 16:00 - 22:00",
+            },
+            {
+              title: "이메일 문의",
+              info: "sseongl6w6l@naver.com",
+              subInfo: "24시간 접수 가능",
+            },
+            {
+              title: "위치",
+              info: "경기도 수원시",
+              subInfo: "화서 스타필드",
+            },
+          ].map((item, index) => (
+            <div
+              key={index}
+              className="bg-white p-6 rounded-xl shadow hover:shadow-md transition-shadow duration-300 text-center"
+            >
+              <h3 className="text-xl font-bold mb-2">{item.title}</h3>
+              <p className="text-gray-600">{item.info}</p>
+              <p className="text-gray-500 text-sm">{item.subInfo}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className="mb-12 max-w-4xl mx-auto">
+          <div className="bg-white shadow rounded-xl overflow-hidden">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3174.3277033124086!2d126.98863257692946!3d37.28736647211236!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357b4367be4ee657%3A0x8a8b001a29248155!2z7Iqk7YOA7ZWE65OcIOyImOybkA!5e0!3m2!1sko!2skr!4v1764166995224!5m2!1sko!2skr"
+              width="100%"
+              height="400"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+              className="w-full h-[400px] md:h-[600px] lg:h-[600px]"
+            ></iframe>
+          </div>
+        </div>
+
+        <div className="mt-12 text-center">
+          <Link
+            to="/contact"
+            className="inline-block px-10 py-3 text-lg font-medium text-white bg-gray-800 rounded-lg shadow hover:bg-gray-800 transition-all duration-300 ease-in-out hover:shadow-lg"
+          >
+            문의하기
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- inflearn_reactnodejs/frontend/src/Page/MainPage/Hero.jsx
+++ inflearn_reactnodejs/frontend/src/Page/MainPage/Hero.jsx
@@ -4,7 +4,7 @@ import HeroImage from "../../assets/kiki_image1.png";
 const Hero = () => {
   return (
     <div className="relative min-h-[110vh] bg-gradient-to-b from-gray-50 to-white pb-0">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-28 lg:py-36">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-24 lg:py-36">
         <div className="flex flex-col lg:flex-row items-center justify-between gap-12">
           <div className="flex-1 text-center lg:text-left">
             <h1 className="text-3xl sm:text-3xl 2xl:text-4xl font-bold text-gray-900 leading-tight mb-6 lg:mb-12">
@@ -20,7 +20,7 @@ const Hero = () => {
               <button className="px-8 py-4 bg-green-800 text-white rounded-lg hover:bg-green-800 transition-colors duration-300 text-lg font-semibold shadow-lg hover:shadow-xl">
                 상담 신청하기
               </button>
-              <button className="px-8 py-4 bg-white text-green-6800 rounded-lg border-2 border-green-800 hover:bg-blue-50 transition-colors duration-300 text-lg font-semibold">
+              <button className="px-8 py-4 bg-white text-green-800 rounded-lg border-2 border-green-800 hover:bg-blue-50 transition-colors duration-300 text-lg font-semibold">
                 더 알아보기
               </button>
             </div>
