9d85bce48a5731fd9fa8ab1991f8fcff7e0973a7
seongjihyeon <sseongl6w6l@naver.com>
2025-11-28 00:28:50 +0900

    update pages

--- inflearn_reactnodejs/frontend/src/Page/About/About.jsx
+++ inflearn_reactnodejs/frontend/src/Page/About/About.jsx
@@ -1,11 +1,100 @@
-import React from 'react'
+import React from "react";
+import TotoroImage from "../../assets/totoro_image1.jpeg";
 
 const About = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="container mx-auto px-4 py-36 max-w-7xl">
+      <div className="relative rounded-2xl overflow-hidden shadow-2xl mb-24">
+        <img src={TotoroImage} className="w-full h-full object-cover" />
+        {/* inset-0 요소를 부모 요소의 경계에 완전히 맞춥니다. (absolute, fixed, relative가 필요) */}
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-slate-900"></div>
+        <div className="absolute bottom-8 left-8 md:bottom-12 md:left-12 text-white">
+          <h3 className="text-2xl md:text-4xl font-bold mb-2 md:mb-3">
+            Zizi Company
+          </h3>
+          <p className="text-base md:text-xl font-light">
+            당신을 지브리의 세계로 안내합니다.
+          </p>
+        </div>
+      </div>
 
-export default About
+      <div className="mb-24 max-w-4xl mx-auto">
+        <h2 className="text-4xl font-bold mb-8 text-slate-800 text-center">
+          회사 소개
+        </h2>
+        <div className="text-lg leading-relaxed text-gray-600 space-y-6">
+          <p>
+            Zizi Company는 지브리를 소개합니다. 
+            스튜디오 지브리는 1985년에 설립된 일본의 애니메이션 제작사로, 미야자키 하야오, 타카하타 이사오 등의 감독이 만든 《센과 치히로의 행방불명》, 《이웃집 토토로》 등 세계적으로 명성 높은 작품들을 선보였습니다. 
+            컴퓨터 그래픽보다는 손그림으로 만든 따뜻한 영상미와 자연, 인간, 평화 등을 중시하는 주제 의식이 특징입니다. 
+          </p>
+          <p>
+            스튜디오 지브리'의 명칭은 사하라 사막에 부는 열풍을 뜻하는 리비아어 'ghibli'에서 유래하였으며, 제2차 세계 대전 중 이탈리아의 비행기의 이름이기도 합니다.
+            미야자키 하야오의 생각에서 '지브리'가 되었으나, 원어에 가까운 발음은 '기블리'입니다.
+          </p>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mb-24">
+        {[
+          { title: "캐릭터", desc: "다양한 캐릭터들의 성격" },
+          { title: "작화", desc: "상상력을 자극하는 작화" },
+          { title: "성장", desc: "캐릭터들의 지속적인 성장과정" },
+        ].map((value, index) => (
+          <div
+            key={index}
+            className="bg-white p-10 rounded-xl shadow-lg text-center hover:shadow-2xl transition-all duration-300 border border-gray-100"
+          >
+            <h3 className="text-2xl font-bold mb-4 text-amber-900">
+              {value.title}
+            </h3>
+            <p className="text-gray-600 text-lg">{value.desc}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="mb-24 max-w-4xl mx-auto text-center">
+        <h2 className="text-4xl font-bold mb-8 text-slate-800">회사 비전</h2>
+        <p className="text-2xl leading-relaxed text-gray-600 font-light">
+          "2030년까지 글로벌 시장을 선도하는 혁신 기업으로 도약하여,
+          <br />더 나은 세상을 만드는데 기여하겠습니다."
+        </p>
+      </div>
+
+      <div className="mb-12">
+        <h2 className="text-4xl font-bold mb-12 text-slate-800 text-center">
+          회사 연혁
+        </h2>
+        <div className="space-y-12 max-w-5xl mx-auto">
+          {[
+            { year: "2023", event: "글로벌 시장 진출" },
+            { year: "2022", event: "시리즈 B 투자 유치" },
+            { year: "2021", event: "주요 기술 특허 획득" },
+            { year: "2020", event: "회사 설립" },
+          ].map((item, index) => (
+            <div
+              key={index}
+              //flex-row는 왼쪽 flex-row-reverse는 우측에 붙인다는 뜻
+              className={`flex items-center gap-8 ${
+                index % 2 === 0 ? "flex-row" : "flex-row-reverse"
+              }`}
+            >
+              <div className="w-1/2 text-center">
+                <div className="bg-white rounded-xl shadow-lg p-8 hover:shadow-2xl transition-all duration-300 border border-gray-100">
+                  <h3 className="text-2xl font-bold mb-3 text-green-800">
+                    {item.year}
+                  </h3>
+                  <p className="text-gray-700 text-lg">{item.event}</p>
+                </div>
+              </div>
+              <div className="w-4 h-4 bg-amber-800 rounded-full"></div>
+              <div className="w-1/2"></div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- inflearn_reactnodejs/frontend/src/Page/Leadership/Leadership.jsx
+++ inflearn_reactnodejs/frontend/src/Page/Leadership/Leadership.jsx
@@ -1,11 +1,160 @@
-import React from 'react'
+import React from "react";
+import Human1 from "../../assets/basic_human.jpg";
 
 const Leadership = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const executives = [
+    {
+      name: "SJH",
+      position: "COO",
+      description:
+        "웹 개발자가 되고 싶어요",
+      imageUrl: Human1,
+    },
+    {
+      name: "KCL",
+      position: "CTO",
+      description: "부자가 되고 싶어요",
+      imageUrl: Human1,
+    },
+    {
+      name: "MCW",
+      position: "CFO",
+      description:
+        "로또 맞고 싶어요",
+      imageUrl: Human1,
+    },
+    {
+      name: "NSJ",
+      position: "CMO",
+      description:
+        "퇴사하고 싶어요",
+      imageUrl: Human1,
+    },
+  ];
 
-export default Leadership
+  const teamMembers = [
+    {
+      name: "정과장",
+      position: "개발팀장",
+      description: "신제품 개발 및 기술 혁신을 담당하고 있습니다.",
+      imageUrl: Human1,
+    },
+    {
+      name: "최과장",
+      position: "영업팀장",
+      description: "글로벌 시장 개척 및 고객 관리를 담당하고 있습니다.",
+      imageUrl: Human1,
+    },
+    {
+      name: "한과장",
+      position: "품질관리팀장",
+      description: "제품 품질 향상 및 품질 시스템 관리를 담당하고 있습니다.",
+      imageUrl: Human1,
+    },
+    {
+      name: "김과장",
+      position: "인사팀장",
+      description: "인사 정책 수립 및 인사 관리를 담당하고 있습니다.",
+      imageUrl: Human1,
+    },
+  ];
+
+  return (
+    <div className="container max-w-7xl mx-auto px-4 py-32">
+      <div className="text-center mb-12">
+        <h1 className="text-4xl lg:text-5xl font-bold text-gray-800 mb-4">
+          임원진 소개
+        </h1>
+        <p className="text-xl text-gray-600">
+          2D의 성장을 이끄는 Zizi_Neko Company의 리더쉽
+        </p>
+      </div>
+
+      <div className="flex flex-col md:flex-row gap-12 mb-24 items-center">
+        <div className="md:w-2/3">
+          <h2 className="text-3xl font-bold text-gray-800 mb-6">CEO 인사말</h2>
+          <div className="text-lg text-gray-600 space-y-6">
+            <p>안녕하십니까, Zizi_Neko Company 대표이사 성대표입니다.</p>
+            <p>
+              저희 Zizi_Neko Company는 20년 이상의 전기 산업 경력을 바탕으로, 혁신적인
+              기술과 서비스를 통해 고객 여러분께 최상의 가치를 제공하기 위해
+              노력하고 있습니다.
+            </p>
+            <p>
+              급변하는 글로벌 시장 환경 속에서도 지속적인 연구개발과 품질 혁신을
+              통해 세계 최고 수준의 제품과 서비스를 제공하겠습니다.
+            </p>
+            <p className="font-semibold mt-8">
+              Zizi_Neko Company 대표이사 성대표 드림
+            </p>
+          </div>
+        </div>
+        <div className="md:w-1/3">
+          <div className="rounded-xl overflow-hidden shadow-lg">
+            <img src={Human1} className="w-full aspect-[3/4] object-cover" />
+            <div className="p-4 bg-white">
+              <h3 className="text-xl font-bold text-gray-800">성대표</h3>
+              <p className="text-gray-800">대표이사</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mb-24">
+        <h2 className="text-3xl font-bold text-gray-800 mb-12 text-center">
+          경영진
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          {executives.map((executive, index) => (
+            <div
+              key={index}
+              className="bg-white rounded-xl shadow-lg overflow-hidden hover:shadow-2xl transition-shadow duration-300"
+            >
+              <div className="aspect-square bg-gray-200">
+                <img
+                  src={executive.imageUrl}
+                  alt={executive.name}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <div className="p-6">
+                <h3 className="text-2xl font-bold text-gray-800 mb-2">{executive.name}</h3>
+                <p className="text-gray-600 font-semibold mb-4">{executive.position}</p>
+                <p className="text-gray-400">{executive.description}</p>
+                </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="mb-24">
+        <h2 className="text-3xl font-bold text-gray-800 mb-12 text-center">
+          핵심 구성원
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          {teamMembers.map((teamMember, index) => (
+            <div
+              key={index}
+              className="bg-white rounded-xl shadow-lg overflow-hidden hover:shadow-2xl transition-shadow duration-300"
+            >
+              <div className="aspect-square bg-gray-200">
+                <img
+                  src={teamMember.imageUrl}
+                  alt={teamMember.name}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <div className="p-6">
+                <h3 className="text-2xl font-bold text-gray-800 mb-2">{teamMember.name}</h3>
+                <p className="text-gray-600 font-semibold mb-4">{teamMember.position}</p>
+                <p className="text-gray-400">{teamMember.description}</p>
+                </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Leadership;
